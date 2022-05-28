968bb1a01d9f4b529e29d8aba952dbfe5617ca8a
george <luiscarrillo129@gmail.com>
2022-05-28 08:59:11 -0600

    a

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 echo 'hello world!'
 cd nclouds-docker/frontend/ && npm start &
-sleep 10 && cd nclouds-docker/backend/ && node server.js 
+sleep 60 && cd nclouds-docker/backend/ && node server.js 
 echo 'hello world!2'
  
