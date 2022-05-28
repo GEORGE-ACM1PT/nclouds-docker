73c7e1ee748374c21d34f60264355bbbe26d086f
george <luiscarrillo129@gmail.com>
2022-05-28 00:53:51 -0600

    r

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,10 +1,24 @@
 const dbConfig = require("../config/db.config");
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  operatorsAliases: false,
-});
+var sequelize ;
+var flag=true;
+
+while(flag)
+{
+ try {
+  sequelize=
+  new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    operatorsAliases: false,
+  });
+  flag =false
+ } catch (error) {
+   
+ }
+}
+
+
 const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
